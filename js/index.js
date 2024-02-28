6a3a767cba86b48f057a5a0c9521d7ab9e929a84
Mehebul02 <mehebul2123@gmail.com>
2024-02-28 19:05:25 +0600

    phone setup

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
-const loadPhone =async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
-    const phones =data.data
-//   console.log(phones) 
-  displayPhone(phones)
-    
-}
-const displayPhone = phones => {
-    const phoneContainer = document.getElementById('phone_card_container');
-    phones.forEach(phone => {
-        console.log(phone);
-        const phoneCard = document.createElement('div');
-        phoneCard.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
-        phoneCard.innerHTML = `<figure><img src='${phone.image}' alt="Shoes" /></figure>
+const loadPhone = async (searchText=12) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  //   console.log(phones)
+  displayPhone(phones);
+};
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone_card_container");
+  phones= phones.slice(0,12)
+  phoneContainer.innerText=''
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+   
+    phoneCard.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
+    phoneCard.innerHTML = `<figure><img src='${phone.image}' alt="Shoes" /></figure>
         <div class="card-body">
           <h2 class="card-title text-black font-semibold">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -20,11 +24,18 @@ const displayPhone = phones => {
           <div class="card-actions justify-center">
             <button class="btn btn-primary">Buy Now</button>
           </div>
-        </div>`
-        phoneContainer.appendChild(phoneCard)
-        
-    });
-}
-loadPhone()
-
-
+        </div>`;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+// searchHandle
+const searchHandle = () => {
+    const searchText =document.getElementById('input_field').value;
+    console.log(searchText);
+    loadPhone(searchText);
+};
+// loadPhone();
+function scrollToSection() {
+    const section = document.getElementById("phone_card_container");
+    section.scrollIntoView({ behavior: "smooth" });
+  }
