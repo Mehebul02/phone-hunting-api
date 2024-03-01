4cda2f572737a189aaf1676d5fc2f27682874eff
Mehebul02 <mehebul2123@gmail.com>
2024-03-01 11:15:45 +0600

    loading added

--- js/index.js
+++ js/index.js
@@ -1,41 +1,49 @@
-const loadPhone = async (searchText=12) => {
+
+const loadPhone = async (searchPhone) => {
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`
   );
   const data = await res.json();
   const phones = data.data;
-  //   console.log(phones)
   displayPhone(phones);
 };
 const displayPhone = (phones) => {
+  
   const phoneContainer = document.getElementById("phone_card_container");
-  phones= phones.slice(0,12)
-  phoneContainer.innerText=''
+  phones =phones.slice(0,12)
+  phoneContainer.innerText = ''
   phones.forEach((phone) => {
-    console.log(phone);
-    const phoneCard = document.createElement("div");
+    
+    const div = document.createElement("div");
    
-    phoneCard.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
-    phoneCard.innerHTML = `<figure><img src='${phone.image}' alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title text-black font-semibold">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <p class ='text-2xl font-bold'>$996</p>
-          <div class="card-actions justify-center">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>`;
-    phoneContainer.appendChild(phoneCard);
+    div.classList = `card w-96 bg-base-100 shadow-xl`;
+    div.innerHTML = `<figure><img src='${phone.image}'alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-center">
+        <button  class="btn btn-primary ">Details</button>
+      </div>
+    </div>`;
+    phoneContainer.appendChild(div);
+    
   });
+ toggleLoading(false)
 };
-// searchHandle
 const searchHandle = () => {
-    const searchText =document.getElementById('input_field').value;
-    console.log(searchText);
-    loadPhone(searchText);
+  toggleLoading(true)
+  const searchInputText = document.getElementById("input_field").value;
+  loadPhone(searchInputText);
 };
-// loadPhone();
-function scrollToSection() {
-    const section = document.getElementById("phone_card_container");
-    section.scrollIntoView({ behavior: "smooth" });
+// spinner Loading 
+const toggleLoading = (isLoading) => {
+      const toggleLoading = document.getElementById('toggle_loading');
+      if(isLoading){
+        toggleLoading.classList.remove('hidden')
+      }
+      else{
+        toggleLoading.classList.add('hidden')
+      }
+      
   }
+// loadPhone();
